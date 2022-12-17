a4e6309ec4a9fed6a314cbfd452216dde272d330
Park-Jeong-Ji <jgpark1998@gmail.com>
2022-12-17 18:28:03 +0900

    fix: fix elif %2 -> %3

--- main.py
+++ main.py
@@ -176,10 +176,10 @@ while True:
     for idx, tile in enumerate(my_units):
         if idx % 3 == 0 or idx % 3 == 1:
             target = get_nearest_opp_unit(tile)
-        elif idx % 2 == 2:
+        elif idx % 3 == 2:
             target = get_nearest_neutral_unit(tile)
         if target:
-            amount = 0  # TODO: pick amount of units to move
+            amount = tile.units  # TODO: pick amount of units to move
             actions.append("MOVE {} {} {} {} {}".format(amount, tile.x, tile.y, target.x, target.y))
 
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
